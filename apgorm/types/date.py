0e2137dbd3e050c9baa959784e287ad59547b46e
Circuit <circuitsacul@gmail.com>
2021-12-13 16:09:02 -0500

    add timestampz and timez types

--- apgorm/types/date.py
+++ apgorm/types/date.py
@@ -9,8 +9,16 @@ class Timestamp(SqlType[datetime.datetime]):
     sql_name = "TIMESTAMP"
 
 
+class TimestampZ(SqlType[datetime.datetime]):
+    sql_name = "TIMESTAMP WITH TIME ZONE"
+
+
 class Time(SqlType[datetime.time]):
     sql_name = "TIME"
+
+
+class TimeZ(SqlType[datetime.time]):
+    sql_name = "TIME WITH TIME ZONE"
 
 
 class Interval(SqlType[datetime.timedelta]):
