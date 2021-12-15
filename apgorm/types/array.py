a9b5fd9da460446a4fc5e945a805c2e00a206340
Circuit <circuitsacul@gmail.com>
2021-12-15 10:42:14 -0500

    Update array.py

--- apgorm/types/array.py
+++ apgorm/types/array.py
@@ -8,8 +8,24 @@ _T = TypeVar("_T", bound=SqlType)
 
 
 class Array(SqlType["list[_T]"]):
-    sql_name = "ARRAY"
+    # NOTE: must initialize Array to get sql_name
 
     def __init__(self, type_: _T, size: int | None = None):
         self.type_ = type_
         self.size = size
+
+        def _get_arrays(
+            t: SqlType, depth: int = 1, arrays: list[Array] | None = None
+        ) -> tuple[int, list[Array], SqlType]:
+            arrays = arrays or []
+            if isinstance(t, Array):
+                arrays.append(t)
+                return _get_arrays(t.type_, depth=depth + 1, arrays=arrays)
+            else:
+                return depth, arrays, t
+
+        dimensions, arrays, final = _get_arrays(self.type_)
+
+        self.sql_name = (
+            final.sql_name + "[]" * dimensions
+        )  # FIXME: include size
