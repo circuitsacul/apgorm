906393ba9b728f1a0206c65f401f28c7686ac9b1
Circuit <circuitsacul@icloud.com>
2022-01-07 11:46:05 -0500

    docstrings for monetary.py

--- apgorm/types/monetary.py
+++ apgorm/types/monetary.py
@@ -26,4 +26,10 @@ from .base_type import SqlType
 
 
 class Money(SqlType[str]):
+    """Currency type, 8 bytes, range -92233720368547758.08 to
+    +92233720368547758.07.
+
+    https://www.postgresql.org/docs/14/datatype-money.html
+    """
+
     sql = "MONEY"
