37cb0d8866a4b8dd9544c9ba1c615b3fe09c7dea
Circuit <circuitsacul@icloud.com>
2022-01-08 15:17:43 -0500

    SP_GIST -> SPGIST

--- apgorm/indexes.py
+++ apgorm/indexes.py
@@ -48,7 +48,7 @@ class IndexType(Enum):
     BTREE = _IndexType("BTREE", multi=True, unique=True)
     HASH = _IndexType("HASH")
     GIST = _IndexType("GIST", multi=True)
-    SP_GIST = _IndexType("SPGIST")
+    SPGIST = _IndexType("SPGIST")
     GIN = _IndexType("GIN", multi=True)
     BRIN = _IndexType("BRIN", multi=True)
 
