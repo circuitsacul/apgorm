515d42f660bf7985fa3a1fd36991e45b9599a8cb
Circuit <circuitsacul@icloud.com>
2022-01-10 14:55:57 -0500

    Testing for LazyList

---
+++ tests/unittests/utils/test_lazy_list.py
@@ -0,0 +1,43 @@
+# MIT License
+#
+# Copyright (c) 2021 TrigonDev
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import annotations
+
+from apgorm.utils.lazy_list import LazyList
+
+
+def test_lazy_list():
+    initial = range(0, 500)
+    ll = LazyList(initial, str)
+
+    assert len(initial) == len(ll)
+    assert list(ll[0:5]) == [str(v) for v in initial[0:5]]
+    assert ll[6] == str(initial[6])
+    assert list(ll) == [str(v) for v in initial]
+
+    assert repr(ll)  # essentially make sure it doesn't crash
+
+    sll = LazyList([0, 1], str)
+    assert repr(sll)
+
+    mll = LazyList([0, 1, 2, 3, 4], str)
+    assert repr(mll)
